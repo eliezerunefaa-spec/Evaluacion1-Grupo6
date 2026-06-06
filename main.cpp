6ab7404613e4879c09dc150147cc0e25bc021346
jenniretgonzalezcabrera45-crypto <jenniretgonzalezcabrera45@gmail.com>
2026-06-06 08:10:32 -0400

    Se añadió apellido en main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ cout << "2. Eliezer Padrón CI: 30865887" <<endl;
 cout << "3. Diego Alvarado CI: 30288527" <<endl;
 cout << "4. Flor Del Valle Mota CI: 30381658" <<endl;
 cout << "5. Xavier Tirado CI: 31304244 " <<endl;
-cout << "6. Jenniret González Cabrera CI: 30801558" <<endl;
+cout << "6. Jenniret González Cabrera CI: 30801558" <<endl; 
 cout << "7. Odalys Escalona Lameda CI: 30954025   " <<endl<<endl;
 
 return 0;
